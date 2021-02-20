f7c423b54b45e85a3869351027afcfbdaca7618b
Artur Parada <paradaartur@gmail.com>
2021-02-20 12:48:04 +0100

    add login_page class

--- locators.py
+++ locators.py
@@ -4,3 +4,7 @@ class HomePageLocators:
 
     ZALOGUJ_BTN = (By.LINK_TEXT, 'LOGOWANIE')
     COOKIES_ACCEPT = (By.ID,'onetrust-accept-btn-handler')
+
+
+class LoginPageLocators:
+    REGISTER_BTN = (By.XPATH '//button[@class="button logon-view__alternate-section-button"]')
---
+++ pages/login_page.py
@@ -0,0 +1,12 @@
+from pages.base_pages import BasePage
+from selenium.webdriver.support import expected_conditions as EC
+from locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+
+    def click_register_btn(self):
+
+        wait = WebDriverWait(self.driver, 60)
+        element = wait.until(EC.element_to_be_clickable(LoginPageLocators.REGISTER_BTN))
+        element.click()
