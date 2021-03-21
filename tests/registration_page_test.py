cbde01f13417e30f930eb5d20f1fdd50eea3a5a8
Artur Parada <paradaartur@gmail.com>
2021-03-21 17:58:40 +0100

    add ddt

--- tests/registration_page_test.py
+++ tests/registration_page_test.py
@@ -7,6 +7,8 @@ from tests.base_test import BaseTest
 from pages.home_page import HomePage
 from pages.registration_page import RegistrationPage
 from time import sleep
+import csv
+from ddt import ddt, data, unpack
 
 
 
@@ -21,6 +23,16 @@ valid_zip_code = '90-333'
 valid_town = 'Łódź'
 valid_province = 'ŁÓDZKIE'
 
+def get_data(file_name):
+    rows = []
+    data_file = open(file_name, 'rt')
+    reader = csv.reader(data_file)
+    # Pomijam pierwszy wiersz
+    next(reader, None)
+    for row in reader:
+        rows.append(row)
+    return rows
+@ddt
 class RegistrationPageTest(BaseTest):
 
 
@@ -32,22 +44,25 @@ class RegistrationPageTest(BaseTest):
         lp = LoginPage(self.driver)
         lp.click_register_btn()
 
+    @data(*get_data("ddt_date.csv"))
+    @unpack
     @allure.testcase("without_email_phone_number_adreess")
-    def test_without_email_phone_number_adreess(self):
+    def test_without_email_phone_number_adreess(self, name,surname,pasword,rp_password,zip_cod,town,province ):
         rp = RegistrationPage(self.driver)
-        rp.fill_name(valid_name)
-        rp.fill_surname(valid_surname)
+        rp.fill_name(name)
+        rp.fill_surname(surname)
         #rp.fill_email(valid_email)
-        rp.fill_password(valid_password)
-        rp.fill_rp_password(valid_rp_password)
+        rp.fill_password(pasword)
+        rp.fill_rp_password(rp_password)
         #rp.fill_adreess(valid_adreess)
         #rp.phone_number(valid_phone_number)
-        rp.zip_code(valid_zip_code)
-        rp.town_name(valid_town)
-        rp.choose_province(valid_province)
+        rp.zip_code(zip_cod)
+        rp.town_name(town)
+        rp.choose_province(province)
         rp.click_zl()
         rp.verify_visible_errors(3, ["To pole jest obowiązkowe.","To pole jest obowiązkowe.","To pole jest obowiązkowe.",])
 
+    @unittest.skip("Pomijam")
     @allure.testcase("without_phone_number")
     def test_without_phone_number(self):
         rp = RegistrationPage(self.driver)
@@ -64,6 +79,7 @@ class RegistrationPageTest(BaseTest):
         rp.click_zl()
         rp.verify_visible_errors(1,["To pole jest obowiązkowe."])
 
+    @unittest.skip("Pomijam")
     @allure.testcase("test_without_email")
     def test_without_email(self):
         rp = RegistrationPage(self.driver)
@@ -80,6 +96,7 @@ class RegistrationPageTest(BaseTest):
         rp.click_zl()
         rp.verify_visible_errors(1, ["To pole jest obowiązkowe."])
 
+    @unittest.skip("Pomijam")
     @allure.testcase("without_password")
     def test_without_password(self):
         rp = RegistrationPage(self.driver)
@@ -96,6 +113,7 @@ class RegistrationPageTest(BaseTest):
         rp.click_zl()
         rp.verify_visible_errors(1,["To pole jest obowiązkowe."])
 
+    @unittest.skip("Pomijam")
     @allure.testcase("without_password_and_rppassword")
     def test_without_password_and_rppassword(self):
         rp = RegistrationPage(self.driver)
